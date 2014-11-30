bf608f5b5c898cdf8bbba63d069526825039ca65
Sung Won Cho <mikeswcho@gmail.com>
2014-11-30 16:30:06 +1100

    Add liker? class method. Remove unnecessary self

--- lib/acts_as_liked/liker.rb
+++ lib/acts_as_liked/liker.rb
@@ -11,19 +11,19 @@ module ActsAsLiked
         include ActsAsLiked::Liker::InstanceMethods
       end
 
-      def likeable?
+      def liker?
         true
       end
     end
 
     module InstanceMethods
       def liked?(likeable)
-        self.likes.find_by(likeable_id: likeable.id, likeable_type: likeable.class.base_class.name).present?
+        likes.find_by(likeable_id: likeable.id, likeable_type: likeable.class.base_class.name).present?
       end
 
       def like(likeable)
         unless self.liked?(likeable)
-          self.likes.create(likeable_id: likeable.id, likeable_type: likeable.class.base_class.name)
+          likes.create(likeable_id: likeable.id, likeable_type: likeable.class.base_class.name)
         end
       end
 
